475539abc5c65b528d1999eead5399619cd08fb0
Alex Ilacqua <alex.ilacqua@gmail.com>
2025-06-25 23:36:56 -0400

    Added support for other platforms

--- chrome-extension/extraction.js
+++ chrome-extension/extraction.js
@@ -2,19 +2,35 @@
   const url = window.location.href;
 
   const selectorMap = {
-    "workday": '[data-automation-id="job-posting-details"]',
-    "jobs.apple.com": '#root',
+    "workday": ['[data-automation-id="job-posting-details"]'],
+    "jobs.apple.com": ['#root'],
+    "linkedin.com": ['.job-details-jobs-unified-top-card__container--two-pane'],
+    "indeed.com": ['.jobsearch-InfoHeaderContainer'],
+    "greenhouse.io": ['.job__header', '.body'],
+    "amazon.jobs": ['#job-detail'],
+    "jobs.careers.microsoft.com": ['.SearchJobDetailsCard']
   };
 
-  let matchedSelector = null;
+  let matchedSelectors = null;
   for (const domain in selectorMap) {
     if (url.includes(domain)) {
-      matchedSelector = selectorMap[domain];
+      matchedSelectors = selectorMap[domain];
       break;
     }
   }
 
-  const element = matchedSelector ? document.querySelector(matchedSelector) : document.body;
+  if (!matchedSelectors) {
+    return null;  // no match found
+  }
 
-  return element ? element.innerHTML : null;
+  let combinedHTML = '';
+
+  matchedSelectors.forEach(selector => {
+    const el = document.querySelector(selector);
+    if (el) {
+      combinedHTML += el.innerHTML.trim() + '\n';
+    }
+  });
+
+  return combinedHTML.trim() || null;
 })();
